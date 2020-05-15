2f85a3f99420563431eb52a0ff3ca6f234f573ed
RafaBort <rafaelafonseca.bort2019ri@gmail.com>
2020-05-15 20:51:49 -0300

    exercicío resolvido

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CriancaEducada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite alguma coisa...");
+
+            string frases = Console.ReadLine();
+            string educada = frases.Replace("A","@").Replace("e","$").Replace("i","*").Replace("o","&");
+
+            Console.WriteLine(educada);
+        }
+    }
+}
